596abed1c94487f72641836afcfbf8c1f83d4ad5
Lucas <lucassrod@gmail.com>
2020-09-19 18:57:11 -0300

    Criando ferramentas

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.0.2
 	github.com/gorilla/schema v1.2.0 // indirect
-	github.com/klauspost/compress v1.11.0 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,8 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+)
+
+type Todo struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []Todo{
+	{Id: 1, Name: "Food to dog", Completed: false},
+	{Id: 2, Name: "Food to cat", Completed: false},
+}
+
+// Timer will measure how long it takes before a response is returned
+func Timer() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// start timer
+		start := time.Now()
+		// next routes
+		err := c.Next()
+		// stop timer
+		stop := time.Now()
+		// Do something with response
+		c.Append("Server-Timing", fmt.Sprintf("app;dur=%v", stop.Sub(start).String()))
+		// return stack error if exist
+		return err
+	}
+}
 
 func main() {
-	fmt.Println("ola")
-	fmt.Println("vim-go")
+	app := fiber.New()
+
+	// Default middleware config
+	app.Use(requestid.New())
+
+	// Custom Timer middleware
+	app.Use(Timer())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://gofiber.io, https://gofiber.net",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	if os.Getenv("APP_ENV") == "dev" {
+
+		// Or extend your config for customization
+		app.Use(logger.New(logger.Config{
+			Format:     "${pid} ${status} - ${method} ${path}\n",
+			TimeFormat: "02-Jan-2006",
+			TimeZone:   "America/New_York",
+			Output:     os.Stdout,
+		}))
+	}
+
+	// Default compression config
+	app.Use(compress.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
 }
